d211ff93001ed00015fa27ddd17d6e78c86e0f03
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-28 19:28:25 +0100

    navbar comp

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+      <h1>CRYPTO-MOON</h1>
+      <ul>
+        <li>Chat</li>
+        <li>Market</li>
+        <li>News</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
