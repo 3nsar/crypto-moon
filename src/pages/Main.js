f013943a93c39af7bd9b108a121e7f1ad796a270
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-30 14:08:03 +0100

    added table

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
 
 const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=5&page=1&sparkline=true";
 
@@ -17,15 +19,30 @@ const Main = () => {
 
     
   return (
-    <div>
-        {coins.map((coin)=>(
-            <div className='coin-container' key={coin.id}>
-              <img src={coin.image} alt="coin"/>
-              <p>{coin.name}</p>
-            </div>
-
-        ))}
-    </div>
+        <Table variant="dark" className="coin-container">
+             <thead>
+              <tr>
+               <th>#</th>
+               <th>Coin</th>
+               <th>Price</th>
+               <th>24h</th>
+               <th>Volume</th>
+               <th>Mkt-Cap</th>
+              </tr>
+            </thead>
+            {coins.map((coin)=>(
+            <tbody key={coin.id}>
+              <tr>
+                <td>{coin.market_cap_rank}</td>
+                <td><img src={coin.image} alt="coin"/>{coin.name}</td>
+                <td>{coin.current_price}€</td>
+                <td>{parseFloat(coin.price_change_percentage_24h).toFixed(2)}%</td>
+                <td>{coin.total_volume}€</td>
+                <td>{coin.market_cap}€</td>
+              </tr>
+            </tbody>  
+              ))}
+        </Table>
   )
 }
 
