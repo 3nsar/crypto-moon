2bc09bf689a485d759308588dea7e50b3656ddfc
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-30 13:39:46 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Main from './pages/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      
+      <Router>
       <Navbar />
+      <Routes>
+      <Route exact path="/" element={<Main/> }/> 
+      </Routes>
+      </Router>
+      <Main/>
     </div>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=5&page=1&sparkline=true";
 
 const Main = () => {
+    const [coins, setCoins] = useState([])
+
+    useEffect(()=>{
+      const loadingCoins = async ()=>{
+        const response = await axios.get(url)
+        console.log(response.data)
+        setCoins(response.data)
+      }
+      loadingCoins()
+    },[])
+
+    
   return (
-    <div>Main</div>
+    <div>
+        {coins.map((coin)=>(
+            <div className='coin-container' key={coin.id}>
+              <img src={coin.image} alt="coin"/>
+              <p>{coin.name}</p>
+            </div>
+
+        ))}
+    </div>
   )
 }
 
