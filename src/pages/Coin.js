fc5789aa7ae9f5130d60e5c3ae6c1c0180775b28
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-31 20:20:31 +0100

    commit

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,23 +1,29 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import useAxios from '../hooks/useAxios';
+import HistoryChart from './HistoryChart';
+
 
 const Coin = () => {
-  const [info, setInfo] = useState("");
-  const { id } = useParams();
+    const [info, setInfo] = useState("");
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams()
 
-  useEffect(()=>{
-    const loadData = async ()=>{
-       const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
-       console.log(response.data) 
-       setInfo(response.data);
-    }
-    loadData()
- },[]);
- 
-  return (
-    <div className='singlecoin-container'>
+    useEffect(()=>{
+     const loadData = async ()=>{
+         setLoading(true)
+        const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
+        console.log(response.data) 
+        setInfo(response.data);
+        setLoading(false)
+     }
+     loadData()
+  },[]);
+  
+
+
+    return(  
+      <div className='singlecoin-container'>
         <div className='singlecoin-title'> 
            <img src={info.image.large}/>
            <h1>{info.name}</h1>
@@ -25,8 +31,9 @@ const Coin = () => {
         </div>
         <h1>What is {info.name}</h1>
         <div className='singlecoin-text' dangerouslySetInnerHTML={{__html: info.description.en}}/>
-    </div>
-  )
+        <HistoryChart/>
+     </div>
+    )
 }
 
 export default Coin
