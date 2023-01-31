f92156693b60cffe57f837cf18dc4ae3842b408d
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-31 19:17:50 +0100

    added Coin comp

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import useAxios from '../hooks/useAxios';
 
 const Coin = () => {
+  const [info, setInfo] = useState("");
+  const { id } = useParams();
+
+  useEffect(()=>{
+    const loadData = async ()=>{
+       const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
+       console.log(response.data) 
+       setInfo(response.data);
+    }
+    loadData()
+ },[]);
+ 
   return (
-    <div>Coin</div>
+    <div> <h1>{info.name}</h1></div>
   )
 }
 
