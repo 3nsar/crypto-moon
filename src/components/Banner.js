35b640c890f6969930f7b5d1f00414c99534e0eb
3nsar <84131267+3nsar@users.noreply.github.com>
2023-02-19 20:59:07 +0100

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import videobg from "../assets/tunnel-64814.mp4"
 const Banner = () => {
   return (
     <div className='banner-content'>
+      <video src={videobg} autoPlay loop muted/>
         <h1>WELCOME TO THE MARKET</h1>
     </div>
   )
