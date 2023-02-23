0f9fa94fd9312c66c8ff6cdf6534f44177c0344b
3nsar <84131267+3nsar@users.noreply.github.com>
2023-02-23 12:15:35 +0100

    connecting with news api

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const News = () => {
+  const url = 'https://saurav.tech/NewsAPI/top-headlines/category/health/in.json';
+  const [news, setNews] = useState([])
+
+  useEffect(()=>{
+    const loadingCoins = async ()=>{
+      const response = await axios.get(url)
+      console.log(response.data)
+      setNews(response.data)
+    }
+    loadingCoins()
+  },[])
+
   return (
     <div>News</div>
   )
