76d95c36a2096e4ee0264930fbd244d7e186c7bc
3nsar <84131267+3nsar@users.noreply.github.com>
2023-02-25 20:21:11 +0100

    commit

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
 
 const News = () => {
   const url = 'https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json';
@@ -20,18 +21,23 @@ const News = () => {
     <div>
       {news.map((item)=>{
         return(
+          <a href={item.url}>
           <div className='news-container'>
             <div className='news-content'>
               <img src={item.urlToImage} alt="pic" />
                <div className='news-info'>
                    <h1>{item.title.length > 35 ? item.title.slice(0,35)+"..." : item.title}</h1>
                    <p>{item.description.length > 90 ? item.description.slice(0,90) +"..." : item.description}</p>
+                  
                </div>
             </div>
+            
           </div>
+          </a>
         )
       })}
     </div>
+   
   )
 }
 
